0bde81a738a0967e25d6778c8adc108cbac66bc7
niney <kpeteras@gmail.com>
2020-09-19 17:00:12 +0900

    initial commit

---
+++ src/main/java/kr/co/samplepcb/xp/XpApplication.java
@@ -0,0 +1,13 @@
+package kr.co.samplepcb.xp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XpApplication.class, args);
+    }
+
+}
